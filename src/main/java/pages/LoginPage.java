bb7d9cb35dd231a04ebb705dff388498669f9bc9
fakhrulhasan2002 <fakhrulhasan2002@gmail.com>
2020-08-30 21:12:21 -0400

    changes LoginPage.java file

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
+// This is Login Page of page factory
 	private WebDriver dr;
 	public LoginPage(WebDriver driver)
 	{
